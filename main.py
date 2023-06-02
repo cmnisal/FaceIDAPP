59aaf1f705704a25729005858b9398c3805bd3ef
martlgap <martin_knoche04@yahoo.de>
2023-06-02 23:33:13 +0200

    working on track

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ import cv2
 from skimage.transform import SimilarityTransform
 from types import SimpleNamespace
 from sklearn.metrics.pairwise import cosine_distances
+from aiortc import VideoStreamTrack
+import asyncio
+
 
 class Grabber(object):
     def __init__(self, video_receiver) -> None:
         self.currentFrame = None
         self.capture = video_receiver
-        self.thread = threading.Thread(target=self.update_frame)
-        self.thread.daemon = True
+        self.grabber_thread = threading.Thread(target=self.update_frame)
+        self.grabber_thread.daemon = True
 
     def update_frame(self) -> None:
         while True:
@@ -27,8 +30,7 @@ class Grabber(object):
 
     def get_frame(self) -> av.VideoFrame:
         return self.currentFrame
-
-
+    
 # Similarity threshold for face matching
 SIMILARITY_THRESHOLD = 1.2
 
@@ -47,20 +49,53 @@ ctx_container = st.container()
 stream_container = st.empty()
 
 # Start streaming component
-with ctx_container:
-    ctx = webrtc_streamer(
-        key="LiveFaceRecognition",
-        mode=WebRtcMode.SENDONLY,
-        rtc_configuration={"iceServers": ICE_SERVERS},
-        media_stream_constraints={"video": {"width": 1920}, "audio": False},
-    )
+
+ctx = webrtc_streamer(
+    key="LiveFaceRecognition",
+    mode=WebRtcMode.SENDONLY,
+    rtc_configuration={"iceServers": ICE_SERVERS},
+    media_stream_constraints={"video": {"width": 1920}, "audio": False},
+)
 
 # Initialize frame grabber
 grabber = Grabber(ctx.video_receiver)
 
+
+class ImageRenderingTrack(VideoStreamTrack):
+    def __init__(self):
+        super().__init__()
+        self.queue = asyncio.Queue(10)
+
+    async def add_image(self, img: np.ndarray):
+        await self.queue.put(img)
+
+    async def recv(self):
+        img = await self.queue.get()
+        frame = av.VideoFrame.from_ndarray(img, format="bgr24")
+        pts, time_base = await self.next_timestamp()
+        frame.pts = pts
+        frame.time_base = time_base
+        return frame
+
+video_track = ImageRenderingTrack()
+
+ctx2 = webrtc_streamer(
+    key="LiveFaceRecognition2",
+    mode=WebRtcMode.RECVONLY,
+    source_video_track=ctx.video_track,
+    desired_playing_state=ctx.state.playing,
+    rtc_configuration={"iceServers": ICE_SERVERS},
+    media_stream_constraints={"video": {"width": 1920}, "audio": False},
+)
+
+grabber.thread.start()
+
+
+
+
 if ctx.state.playing:
     # Start frame grabber in background thread
-    grabber.thread.start()
+    
 
     # Start main loop
     while True:
@@ -70,4 +105,4 @@ if ctx.state.playing:
             frame = frame.to_ndarray(format="rgb24")
 
             # Show Stream
-            stream_container.image(frame, channels="RGB")
+            video_track.add_image(frame)
